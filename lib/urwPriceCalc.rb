a9326177f5642cc8a38e66f221a5ea257c3e4088
Brian Jerardi <bjerardi@gmail.com>
2013-03-30 13:40:17 -0400

    Add and generate RDoc Documnetaion

--- lib/urwPriceCalc.rb
+++ lib/urwPriceCalc.rb
@@ -1,18 +1,29 @@
+# The program takes command line paramters and manages items
+# for the game urw
+#
+# Author::	Brian Jerardi
 
 require "rubygems"
 require "active_record"
 require "active_support"
 
+# This module contains all functions and classes for the urwp program
 module URWPriceCalc
 
 	module_function
 
+	# Destroy all data in the database
 	def destroy
 		Models::Quality.destroy_all
 		Models::ItemPrice.destroy_all
 		Models::Inventory_Item.destroy_all
 	end
 
+	# Add a Quality to the db
+	#
+	# Takes:
+	# * description - The description of the quality (ex: poor)
+	# * multiplier - The multiplier associated with the quality (ex: 0.5)
 	def addQuality(description, multiplier)
 		puts "Adding: #{description} #{multiplier}"
 		Models::Quality.create(
@@ -20,10 +31,16 @@ module URWPriceCalc
 			:multiplier => multiplier)
 	end
 
+	# Show all Qualities in the database
 	def showQualities
 		Models::Quality.find(:all).map(&:to_s)
 	end
 
+	# Add an Item with it's price to the database
+	#
+	# Takes:
+	# * itemName - The name of the item (ex; spear)
+	# * itemPrice - The price of the item (ex: 344)
 	def addItemPrice(itemName, itemPrice)
 		puts "Adding: #{itemName} #{itemPrice}"
 		Models::ItemPrice.create(
@@ -31,10 +48,16 @@ module URWPriceCalc
 			:price => itemPrice)
 	end
 
+	# Show all Item Prices int he database
 	def showItemPrices
 		Models::ItemPrice.find(:all).map(&:to_s)
 	end
 
+	# Add an invetory item to the database
+	#
+	# Takes:
+	# * itemQuality - The quality of the item (ex: poor)
+	# * itemName - The name of the item (ex: spear)
 	def addInventory(itemQuality, itemName)
 		puts "Adding: #{itemQuality} #{itemName}"
 		Models::Inventory_Item.create(
@@ -42,6 +65,7 @@ module URWPriceCalc
 			:itemPrice_id => Models::ItemPrice.find_by_name(itemName).id)	
 	end
 
+	# Show all the Inventory Items
 	def showInventory
 		items = Models::Inventory_Item.find(:all, 
 						    :group => "quality_id,itemPrice_id"
@@ -50,6 +74,10 @@ module URWPriceCalc
 	#					      :group => "quality_id,itemPrice_id")
 	end		
 
+	# Process the command line arguments
+	#
+	# Takes:
+	# * cmd - an array of command line arguments (ex: i)
 	def process_command(cmd)
 		args = Array(cmd)
 		command = args.shift
@@ -75,10 +103,12 @@ module URWPriceCalc
 		end
 	end
 
+	# Displays "Unrecognized command"
 	def unreqCommand
 		puts "Unrecognized command"
 	end
 
+	# Connect the the database
 	def connect(dbfile="data/urwPriceCalc.db")
 		ActiveRecord::Base.establish_connection(
 			:adapter => "sqlite3",
@@ -86,27 +116,55 @@ module URWPriceCalc
 		)
 	end
 
+	# Module containg the Data Models
 	module Models
+
+		# Class for a Quality Model
 		class Quality < ActiveRecord::Base
+			
+			# To string method for a Quality Model
+			#
+			# Ex:
+			# 6. poor	0.5
 			def to_s
 				"#{id}. #{description} \t #{multiplier}"
 			end
 
 		end
 
+		# Class for an ItemPrice Model
 		class ItemPrice < ActiveRecord::Base
+		
+			# To string method for an ItemPrice Model
+			#
+			# Ex:
+			# 1. spear	344	
 			def to_s
 				"#{id}. #{name} \t #{price}"
 			end
 		end
 
+		# Class for an Invetory_Item Model
 		class Inventory_Item < ActiveRecord::Base
 			belongs_to :quality
 			belongs_to :itemPrice
+
+			# Gets the true price of the item by multipling the
+			# price by the multiplier
+			#
+			# Sample:
+			# a poor spear
+			# * multiplier: 	0.5
+			# * price:	344
+			# * returns:	172
 			def truePrice
 				quality.multiplier*itemPrice.price
 			end
-				
+			
+			# To string method for an Inventory_Item Model
+			#
+			# Ex:
+			# 1. poor	spear	
 			def to_s
 				"#{id}. #{quality.description} #{itemPrice.name}: \t" + 
 				truePrice.to_s
@@ -115,6 +173,7 @@ module URWPriceCalc
 
 		module_function
 
+		# Generates the database schema
 		def generate_schema
 			ActiveRecord::Schema.define do
 				create_table :qualities do |t|
